b871be9274c4759b414255f792bae50d78dc6d2e
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2024-11-06 10:50:14 -0500

    changes

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafíos Fitness</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link href="./css/Tailwind.css" rel="stylesheet">
+    <link href="./colors/Tailwind.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Prata&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
@@ -119,7 +119,7 @@
         </div>
     </footer>
 
-    <script src="./scripts/carrusel.js"></script>
-    <script src="./scripts/modal.js"></script>
+    <script src="./JS/carrusel.js"></script>
+    <script src="./JS/modal.js"></script>
 </body>
 </html>
