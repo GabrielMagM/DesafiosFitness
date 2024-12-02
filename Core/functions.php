43e153e522cc841f52fe04522bfb1077b6ce22a5
Gabriel_MagMed <gabriel.t.13@hotmail.com>
2024-12-02 11:59:12 -0500

    Nuevos cambios

---
+++ Core/functions.php
@@ -0,0 +1,13 @@
+<?php 
+include '../config/Conexion.php';
+
+class Functions extends Conexion{
+
+    //---------AQUI SE IMPLEMENTE EL REGISTRAR - INICIAR SESION-------------//
+
+    public function registerUser(){
+        
+    }
+
+}
+?>
